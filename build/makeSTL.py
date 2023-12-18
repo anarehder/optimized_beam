544da602896fc12e88786f33fae02ab62e570dff
anarehder <anapaula.rehder@gmail.com>
2023-12-18 15:25:19 -0300

    fix remove borders from makeSTL

--- build/makeSTL.py
+++ build/makeSTL.py
@@ -1,25 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
-import sys
 import vtk
 
-filename = 'DISP_TESTE.vtu'
-    
-reader = vtk.vtkXMLUnstructuredGridReader() #VTU
-reader.SetFileName(filename)
+# Carregar o arquivo VTU
+reader = vtk.vtkXMLUnstructuredGridReader()
+reader.SetFileName("DISP_TESTE.vtu")
+reader.Update()
 
-surface_filter = vtk.vtkDataSetSurfaceFilter()
-surface_filter.SetInputConnection(reader.GetOutputPort())
+# Criar uma caixa delimitadora para a área desejada
+bounding_box = vtk.vtkBox()
+bounding_box.SetBounds(-30, 30, -15, 15, -1000, 1000)  # Ajuste conforme necessário
 
-triangle_filter = vtk.vtkTriangleFilter()
-triangle_filter.SetInputConnection(surface_filter.GetOutputPort())
+# Filtro de recorte para a área delimitada pela caixa
+clip_box = vtk.vtkClipDataSet()
+clip_box.SetInputData(reader.GetOutput())
+clip_box.SetClipFunction(bounding_box)
+clip_box.GenerateClippedOutputOn()
+clip_box.SetInsideOut(True)
+clip_box.Update()
 
+# Converter a malha para polígono
+triangulator = vtk.vtkDataSetTriangleFilter()
+triangulator.SetInputData(clip_box.GetOutput())
+triangulator.Update()
+
+# Converter de UnstructuredGrid para PolyData
+geometry_filter = vtk.vtkGeometryFilter()
+geometry_filter.SetInputData(triangulator.GetOutput())
+geometry_filter.Update()
+
+
+# Salvar o resultado em um novo arquivo STL
 writer = vtk.vtkSTLWriter()
-writer.SetFileName('DISP_TESTE.stl')
-writer.SetInputConnection(triangle_filter.GetOutputPort())
-writer.Write()     
-
+writer.SetFileName("DISP_TESTE.stl")
+writer.SetInputData(geometry_filter.GetOutput())
+writer.Write()
